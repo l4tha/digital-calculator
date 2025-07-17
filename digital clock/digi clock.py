0df3936d266980fa0138dfa6bfc6f9434fefadf3
lat <laashh33@gmail.com>
2025-07-17 21:33:25 +0530

    Add files via upload

---
+++ digital clock/digi clock.py
@@ -0,0 +1,12 @@
+import time
+
+input_time = int(input("Enter the time in seconds: "))
+
+for x in range(input_time, 0, -1):
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+
+print("TIME'S UP!!")
